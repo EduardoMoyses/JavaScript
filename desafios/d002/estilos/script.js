7fd380b72a5480b67c5a19d892607af36654d75a
EduardoMoyses <153742466+EduardoMoyses@users.noreply.github.com>
2025-01-04 11:02:47 -0300

    atualizei o script

--- desafios/d002/estilos/script.js
+++ desafios/d002/estilos/script.js
@@ -4,8 +4,8 @@ function verificar() {
     var fano = document.getElementById('ano')
     var res = document.getElementById('res')
 
-    if (ano.value == 0 || fano.value > ano) {
-        window.alert('Verifique os dados e tente novamente')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente')
     } else {
         var fsex = document.getElementsByName('isex')
         var idade = ano - Number(fano.value)
