24c18efa49da6fc945e8522246ed5fd2edfef1cb
EduardoMoyses <153742466+EduardoMoyses@users.noreply.github.com>
2025-01-07 09:08:31 -0300

    criei o exercício 2 do desafio 3

--- desafios/d003/ex002/estilos/script.js
+++ desafios/d003/ex002/estilos/script.js
@@ -1,42 +1,20 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('ano')
-    var res = document.getElementById('res')
+function tabuada() {
+    var num = document.getElementById('num')
+    var tab = document.getElementById('seltab')
 
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente')
+    if (num.value.length == 0) {
+        window.alert('Por favor digite um número!')
     } else {
-        var fsex = document.getElementsByName('isex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            gênero = 'homem'
-            if (idade >= 0 && idade <= 10) {
-                img.setAttribute('src', 'imagens/crianca-m.jpg')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'imagens/jovem-m.jpg')
-            } else if (idade >= 21 && idade < 50) {
-                img.setAttribute('src', 'imagens/adulto-m.jpg')
-            } else if (idade > 50) {
-                img.setAttribute('src', 'imagens/idoso.jpg')
-            }
-        } else if (fsex[1].checked) {
-            gênero = 'mulher'
-            if (idade >= 0 && idade <= 10) {
-                img.setAttribute('src', 'imagens/crianca-f.jpg')
-            } else if (idade < 21) {
-                img.setAttribute('src', 'imagens/adoles-f.jpg')
-            } else if (idade >= 21 && idade < 50) {
-                img.setAttribute('src', 'imagens/adulto-f.jpg')
-            } else if (idade > 50) {
-                img.setAttribute('src', 'imagens/idosa.jpg')
-            }
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-        res.appendChild(img)
     }
+    
 }
