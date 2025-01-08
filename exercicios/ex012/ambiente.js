ce747c4465a7b8d676872699e3f4264f7587ec04
EduardoMoyses <153742466+EduardoMoyses@users.noreply.github.com>
2025-01-08 11:13:05 -0300

    atualizei o exercício 12

--- exercicios/ex012/ambiente.js
+++ exercicios/ex012/ambiente.js
@@ -3,3 +3,10 @@ num.push(1)
 num.sort()
 console.log(`O vetor tem ${num.length} elementos`)
 console.log(`O primeiro elemento é ${num[0]}`)
+
+var pos = num.indexOf(4)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
--- exercicios/ex012/vetor-na-tela.js
+++ exercicios/ex012/vetor-na-tela.js
@@ -8,6 +8,13 @@ console.log(num[1])
 console.log(num[2])
 console.log(num[3])
 console.log(num[4])
+
+
+for (let pos = 0; pos < valores.length;pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
 */
 
-for (let pos = 0; pos < valores.length;pos++)
+for (let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
