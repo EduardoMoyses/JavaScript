dcef40dff089e00b2f7fae2fc66505d96f98480c
EduardoMoyses <153742466+EduardoMoyses@users.noreply.github.com>
2025-01-08 10:27:48 -0300

    criei o exercício 12

---
+++ exercicios/ex012/vetor-na-tela.js
@@ -0,0 +1,13 @@
+let valores = [3,5,6,1,8]
+
+// console.log(vetores)
+
+/*
+console.log(num[0])
+console.log(num[1])
+console.log(num[2])
+console.log(num[3])
+console.log(num[4])
+*/
+
+for (let pos = 0; pos < valores.length;pos++)
